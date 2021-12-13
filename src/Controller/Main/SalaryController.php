97e7a71363431dca251ef9e50f4c228081456587
ivan5420 <i.chelnokov@mykedr.ru>
2021-12-13 13:28:24 +0300

    add contrains for salary Controller

--- src/Controller/Main/SalaryController.php
+++ src/Controller/Main/SalaryController.php
@@ -15,6 +15,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class SalaryController extends AbstractController
 {
 
+    private EntityManagerInterface $em;
+    private $contract_id = null;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+
+    }
+
     /**
      * @Route ("/salary/create/{contract_id}", name="create_salary")
      * @param Request $request
@@ -24,6 +33,26 @@ class SalaryController extends AbstractController
     public function createSalaryByContract(Request $request, EntityManagerInterface $em): Response
     {
         $contract = $em->getRepository(Contract::class)->find($request->get('contract_id'));
+
+        if($contract == null){
+            $this->addFlash(
+                'notice',
+                'Contract is empty'
+            );
+            return $this->redirectToRoute('contract');
+        }
+
+//dd($this->isSalaryIsset($contract->getId()));
+        if(!$this->isSalaryIsset($contract->getId())){
+            $this->addFlash(
+                'notice',
+                'Salary is already exists'
+            );
+            return $this->redirectToRoute('contract');
+        }
+
+
+
         $employee_id = $contract->getEmployeeWorker();
         $sum = $contract->getSumSlopeWork() + $contract->getProductWorkSum()+$contract->getAdditionalWorkSum();
         $salary_type = $em->getRepository(SalaryType::class)->find('2');
@@ -31,7 +60,9 @@ class SalaryController extends AbstractController
         $salary = new Salary();
         $salary->setSalaryType($salary_type);
         $salary->setEmployee($employee_id);
+        $salary->setContract($contract);
         $salary->setSum($sum);
+//        dd($salary->getContract()->getId());
         $em->persist($salary);
         $em->flush();
 
@@ -41,5 +72,14 @@ class SalaryController extends AbstractController
         );
 
         return $this->redirectToRoute('contract');
+    }
+
+    /**
+     * @param int $contract_id
+     * @return boolean
+     */
+    private function isSalaryIsset(int $contract_id): bool
+    {
+        return !$this->em->getRepository(Salary::class)->findOneBy(['contract' => $contract_id]);
     }
 }
