135461ec183a069f4506335c16bd0e5490ea58a8
ivan5420 <i.chelnokov@mykedr.ru>
2021-12-13 10:32:31 +0300

    fix psalm

--- src/Entity/Employees.php
+++ src/Entity/Employees.php
@@ -6,7 +6,7 @@ use App\Repository\EmployeesRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use JetBrains\PhpStorm\Pure;
+
 
 /**
  * @ORM\Entity(repositoryClass=EmployeesRepository::class)
@@ -53,7 +53,7 @@ class Employees
 
 
 
-    #[Pure] public function __toString():string
+    public function __toString():string
     {
         return $this->getName();
     }
